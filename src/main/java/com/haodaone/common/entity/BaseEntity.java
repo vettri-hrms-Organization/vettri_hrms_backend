d36a3ba228f260f95e872f5a2d14f7b1d527f5b1
amvikkash <brokzvicky@gmail.com>
2026-09-16 22:10:07 +0530

    bug fix 1

--- src/main/java/com/haodaone/common/entity/BaseEntity.java
+++ src/main/java/com/haodaone/common/entity/BaseEntity.java
@@ -30,7 +30,7 @@ import java.time.LocalDateTime;
 public abstract class BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Version
